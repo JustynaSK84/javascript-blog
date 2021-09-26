ca5b506ba108ed52b358a90eb7726c8d906d5663
Justyna Sadowska <korzyniewskaj@gmail.com>
2021-09-26 23:56:34 +0200

    javascript-blog

--- js/script.js
+++ js/script.js
@@ -2,30 +2,33 @@
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
 });*/
-const titleClickHandler = function(){
+const titleClickHandler = function(event){
+  event.preventDefault();
+  const clickedElement = this;
   console.log('Link was clicked!');
-  /* remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks) {
     activeLink.classList.remove('active');
   }
 
-  /* add class 'active' to the clicked link */
+  console.log ('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('. list .titles a.active');
 
   for(let activeArticle of activeArticles) {
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log ('href', articleSelector);
+  const targetArticle = document.querySelectorAll(articleSelector);
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  targetArticle.classList.add('active');
 
-  /* add class 'active' to the correct article */
-}
+};
 
 const links = document.querySelectorAll('.titles a');
 
